b5e28fa93e860ff7077bc4b772192164baa1125e
TroupeCrypto <thegungareport@gmail.com>
2025-10-21 14:44:41 -0400

    Delete pages/BibPage.tsx

--- pages/BibPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// This component is obsolete and no longer used in the application.
-// Its functionality has been superseded by PersonaPage.tsx.
-const BibPage: React.FC = () => {
-  return null;
-};
-
-export default BibPage;
